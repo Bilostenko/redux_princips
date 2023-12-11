a2a481cd4e716a0fcd7e4e58d282841aa6339bad
Bilostenko <bilostenkod@gmail.com>
2023-12-11 15:21:41 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div id="root">
+        <h1 id="counter">0</h1>
+        <button id="dec">DEC</button>
+        <button id="inc">INC</button>
+      </div> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,133 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+// import { createStore } from 'redux';
+
+// const initialState = 0;
+
+
+// const reducer = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INC':
+//       return state + 1;
+//     case 'DEC':
+//       return state - 1;
+//       case 'RND':
+//         return state * action.payload
+//     default:
+//       return 0
+//   }
+// }
+
+// const store = createStore(reducer)
+
+// const update = () => {
+// document.getElementById('counter').textContent= store.getState()
+// }
+
+// store.subscribe(() => {
+//   update()
+// })
+
+// document.getElementById('inc').addEventListener('click', () => {
+//   store.dispatch({ type: 'INC' })
+// })
+// document.getElementById('dec').addEventListener('click', () => {
+//   store.dispatch({ type: 'DEC' })
+// })
+// document.getElementById('rnd').addEventListener('click', () => {
+//   let random = Math.random()
+//   store.dispatch({ type: 'RND', payload: random })
+// })
+
+let nextToDoId = 0;
+
+const addTask = text => {
+  return {
+    type: 'ADD_TASK',
+    text,
+    id: nextToDoId++
+  }
+}
+
+const removeTask = id => {
+  return {
+    type: 'REMOVE_TASK',
+    id
+  }
+}
+const initialState = {
+  todos: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: action.id,
+            text: action.text,
+            completed: false,
+          },
+        ],
+      };
+      case 'REMOVE_TASK':
+        return {
+          ...state,
+          todos: state.todos.filter(task => task.id !== action.id),
+        };
+      default:
+        return state;
+  }
+}
+const store = createStore(reducer)
+
+document.getElementById('addTaskBtn').addEventListener('click', () => {
+  const taskInput = document.getElementById('taskInput');
+  const taskText = taskInput.value.trim();
+  if (taskText !== '') {
+    store.dispatch(addTask(taskText));
+    taskInput.value = ''; // Очистка поля введення
+  }
+});
+
+document.getElementById('killTaskBtn').addEventListener('click', (event) => {
+  const taskID = event.currentTarget.getAttribute('data-id');
+  console.log(taskID);
+  store.dispatch(removeTask(taskID));
+});
+
+const render = () => {
+  const taskList = document.getElementById('taskList');
+  taskList.innerHTML = ''; // Очистка списку перед відображенням нового стану
+
+  store.getState().todos.forEach(task => {
+    const listItem = document.createElement('li');
+    const killbtn = document.createElement('button')
+    listItem.textContent = task.text;
+    killbtn.textContent = 'Kill btn'
+    killbtn.className = 'killTaskBtn'
+    listItem.setAttribute('data-id', task.id)
+    taskList.appendChild(listItem);
+    listItem.appendChild(killbtn)
+  });
+};
+
+store.subscribe(render); // Оновлення відображення при зміні стану
+
+render(); // Першочергове відображення початкового стану
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
